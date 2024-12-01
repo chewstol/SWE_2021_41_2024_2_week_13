536de04113d0fab80e4f85eee2e83a0719e50d2a
chewstol <mj9962@g.skku.edu>
2024-12-01 20:06:16 +0900

    if condition removed

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ def train_file_list_to_json(english_file_list: List[str], german_file_list: List
     """Converts two lists of file paths into a list of json strings"""
     # Preprocess unwanted characters
     def process_file(file):
-        if '\\' in file:
+    
             file = file.replace('\\', '\\')
-        if '/' or '"' in file:
+        
             file = file.replace('/', '\\/')
             file = file.replace('"', '\\"')
         return file
